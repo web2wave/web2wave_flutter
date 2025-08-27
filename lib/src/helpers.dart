30057408d9a03e186dbdbca6c0eb05c53e722bc8
Aleksandar Zdravkovic <aleksandar.zdravkovic@aetherius-solutions.com>
2025-08-27 14:07:47 +0200

    Added logic for appending additional queries

--- lib/src/helpers.dart
+++ lib/src/helpers.dart
@@ -2,3 +2,15 @@ bool isValidUrl(String url) {
   Uri? uri = Uri.tryParse(url);
   return uri != null && uri.hasScheme && uri.hasAuthority;
 }
+
+String prepareUrl(String baseUrl, int topOffset, int bottomOffset) {
+    final uri = Uri.parse(baseUrl);
+    
+    final params = Map<String, String>.from(uri.queryParameters);
+    
+    params['webview_flutter'] = '1';
+    params['top_padding'] = topOffset.toString();
+    params['bottom_padding'] = bottomOffset.toString();
+    
+    return uri.replace(queryParameters: params).toString();
+}
